4b69bc399ecbfe3b94fc27caf48abc636672e04e
kauesalvio <kaue.salvio@ambevtech.com.br>
2021-10-03 13:38:55 -0300

    criação do user e alteração do product

--- StoreStock/StoreStock.Domain/Entities/User.cs
+++ StoreStock/StoreStock.Domain/Entities/User.cs
@@ -1,6 +1,14 @@
 ﻿namespace StoreStock.Domain.Entities
 {
-    public class User
+    public class User : BaseEntity
     {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
     }
 }
