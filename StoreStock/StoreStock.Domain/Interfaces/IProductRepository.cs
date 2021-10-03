5944efe7043fda2493ba1cef65ff1657d2a4f88b
kauesalvio <kaue.salvio@ambevtech.com.br>
2021-10-03 13:38:31 -0300

    interfaces do repository

---
+++ StoreStock/StoreStock.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using StoreStock.Domain.Entities;
+
+namespace StoreStock.Domain.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
---
+++ StoreStock/StoreStock.Domain/Interfaces/IProviderRepository.cs
@@ -0,0 +1,8 @@
+﻿using StoreStock.Domain.Entities;
+
+namespace StoreStock.Domain.Interfaces
+{
+    public interface IProviderRepository : IGenericRepository<Provider>
+    {
+    }
+}
